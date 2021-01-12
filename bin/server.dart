57daa2384f6300147a7e7c49e05b9165d8c8242b
Andrey Maksimov <meamka@ya.ru>
2021-01-12 18:46:45 +0300

    Simplify base example

--- bin/server.dart
+++ bin/server.dart
@@ -1,16 +1,8 @@
-import 'dart:io';
-
 import 'package:laska/laska.dart';
 
 void main(List<String> args) async {
-
-  final app = Laska();
-
-  app.GET('/', (Context context) => context.Text('Hello, world.'));
-
-  app.GET('/todos', (Context context) async {
-    throw Exception('Custom Exception throwed.');
-  });
+  final app = Laska()
+    ..GET('/', (context) => context.Text('Hello, world.'));
 
   await app.run();
 }
